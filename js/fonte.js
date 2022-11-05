470809997d85ba1a7294397ca25dc493fa60a4b1
ecolaltojunior <edson.colalto@fatec.sp.gov.br>
2022-11-04 21:44:25 -0300

    aumentar e diminuir fonte

---
+++ js/fonte.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    let fonte = 14
+    $('#aumenta').click(function(){
+        if(fonte<22){
+            //fonte++
+            $('body').css({'font-size': ++fonte+'px'})
+        }
+    })
+    $('#diminui').click(function(){
+        if(fonte>14){
+            $('body').css({'font-size': --fonte+'px'})
+        }
+    })
+})
